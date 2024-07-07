76762dc0794868f9319a53540eddf4185b1ffe52
dmitriykosolobov <kosolobov.2003@gmail.com>
2024-07-07 17:49:47 +0300

    init commit

---
+++ backend/src/main/java/ru/petflower/PetFlowerApplication.java
@@ -0,0 +1,13 @@
+package ru.petflower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFlowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFlowerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/petflower/PetFlowerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petflower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFlowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
