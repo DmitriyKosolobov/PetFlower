412634a5c0edf6059a5ff0b6acda648898d99b92
dmitriykosolobov <kosolobov.2003@gmail.com>
2024-09-12 20:00:52 +0300

    fix http connection

--- backend/src/main/java/ru/petflower/configuration/SecurityConfig.java
+++ backend/src/main/java/ru/petflower/configuration/SecurityConfig.java
@@ -3,7 +3,6 @@ package ru.petflower.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,7 +26,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())
+                )
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authz ->
